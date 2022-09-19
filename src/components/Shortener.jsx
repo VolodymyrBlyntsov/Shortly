23a1c269ab9ccfe085dd5fcbba4929150af2ce8d
Volodymyr <volodymyr.blyntsov@gmail.com>
2022-09-19 16:49:52 +0300

    Shortly
    Work on the 1.1 release in a separate branch.
    
    * Added local links storage (for users - if you want to see your links not in a single instance, see the site code through f12 in the relevant section).
    
    The ability to see only one link - although not cost-effective and not flexability, but it supports the orgonomics of the site.
    
    🚫 Bug detected! When entering arbitrary text into input field, site crashes and shows white screen (I don't know how to fix it at the moment).

--- src/components/Shortener.jsx
+++ src/components/Shortener.jsx
@@ -2,20 +2,29 @@ import React, {useEffect, useState} from 'react'
 import bgDesktop from '../images/bg-shorten-desktop.svg'
 import bgMobile from '../images/bg-shorten-mobile.svg'
 
+const getStoreLinks = () => {
+    let links = localStorage.getItem("links")
+
+    if (links) {
+        return JSON.parse(localStorage.getItem("links"))
+    } else {
+        return []
+    }
+}
+
 const Shortener = () => {
     const [text, setText] = useState("")
-    const [links, setLinks] = useState([])
+    const [links, setLinks] = useState(getStoreLinks())
     const [buttonText, setButtonText] = useState("Copy")
 
     const handleSubmit = (e) => {
         e.preventDefault()
         if (!text) {
-            return
+            console.log("Please, put the link into the input field.")
         } else {
             const shortenLink = async () => {
               const response = await fetch(`https://api.shrtco.de/v2/shorten?url=${text}`)
               const data = await response.json()
-              console.log(data.result)
               setLinks(data.result)
               setText("")
             }
@@ -28,19 +37,22 @@ const Shortener = () => {
         setButtonText("Copied!")
     }
 
+    useEffect(() => {
+        localStorage.setItem("links", JSON.stringify(links))
+    }, [links])
+
     return (
         <>
             <section className="max-width shortener relative">
                 <picture>
                     <source media="(min-width:768px)" srcSet={bgDesktop} />
-                    <img src={bgMobile} alt="" />
+                    <img src={bgMobile} alt="mobile-background-shortener" />
                 </picture>
 
                 <form className="form" onSubmit={handleSubmit}>
                     <div className="flex flex-col md:flex-row">
                         <input onChange={(e) => setText(e.target.value)} value={text} type="url" placeholder="Shorten your link here" className="w-full py-2 px-5 rounded-lg mb-2 md:mb-0 md:w-2/3" />
-                        <button onClick={handleSubmit} className="btn-cta rounded-lg w-full md:w-40 md:ml-5">Shorten It!</button>
-                    </div>
+                        <button onClick={handleSubmit} className="btn-cta rounded-lg w-full md:w-40 md:ml-5">Shorten It!</button>                    </div>
                 </form>
 
                 <div className="flex flex-col items-center justify-center md:bg-slate-100 bg-white rounded-lg text-center md:flex-row md:justify-between p-3 mt-3">
